f0c1bc5021b8fa1b5960e72ace05e1e3400b61d6
MXYAN <89431662+MZY-mar@users.noreply.github.com>
2023-04-17 02:28:47 -0700

    Add files via upload

---
+++ MvcProject/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using ApplicationCore.Entities;
+using ApplicationCore.Model;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using MvcProject.Models;
+
+namespace MvcProject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly EmployeeRepository _repository;
+    public HomeController(ILogger<HomeController> logger, EmployeeRepository repository)
+    {
+        _logger = logger;
+        _repository = repository;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.AnyValue = "This is an item stored in viewbag";
+       IEnumerable<Employees> listemp = _repository.GetAll();
+        return View(listemp);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
